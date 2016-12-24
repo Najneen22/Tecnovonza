703a67e2d25ac485c3524a25831a4d33fc4c7443
daniyal <daniyal@tailoredtech.in>
2016-12-24 14:19:18 +0530

    Added navigation drawer fragment setup

--- app/src/main/java/com/example/tailoredtech/tecnovanza/activities/HomeActivity.java
+++ app/src/main/java/com/example/tailoredtech/tecnovanza/activities/HomeActivity.java
@@ -1,15 +1,43 @@
 package com.example.tailoredtech.tecnovanza.activities;
 
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.tailoredtech.tecnovanza.R;
+import com.example.tailoredtech.tecnovanza.fragments.HomeFragment;
+import com.example.tailoredtech.tecnovanza.fragments.NavigationDrawerFragment;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        toolbar= (Toolbar) findViewById(R.id.app_toolbar);
+
+        NavigationDrawerFragment navigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        navigationDrawerFragment.setUp(R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+
+        attachFragment(new HomeFragment());
+
+    }
+
+
+
+    public void attachFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.add(R.id.fragment_container, fragment);
+        ft.commit();
     }
 }
--- app/src/main/java/com/example/tailoredtech/tecnovanza/fragments/NavigationDrawerFragment.java
+++ app/src/main/java/com/example/tailoredtech/tecnovanza/fragments/NavigationDrawerFragment.java
@@ -1,18 +1,35 @@
 package com.example.tailoredtech.tecnovanza.fragments;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.tailoredtech.tecnovanza.R;
+import com.example.tailoredtech.tecnovanza.adapter.NavigationDrawerAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NavigationDrawerFragment extends Fragment {
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private boolean mUserLearned;
+    private boolean mFromSavedInstanceState;
+    public static final String FILE_NAME = "drawer_pref";
+    public static final String KEY_USER_LEARNED_DRAWER = "user_learned_drawer";
+    private View containerView;
+    private NavigationDrawerAdapter navigationDrawerListAdapter;
+    private static final int FRAGMENT_DELAY = 250;
 
 
     public NavigationDrawerFragment() {
@@ -26,5 +43,57 @@ public class NavigationDrawerFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
     }
+
+    public void setUp(int fragmentId, DrawerLayout drawerLayout, Toolbar toolbar) {
+        mDrawerLayout = drawerLayout;
+        containerView = getActivity().findViewById(fragmentId);
+
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                if (!mUserLearned) {
+                    mUserLearned = true;
+                    saveToPreference(getActivity(), KEY_USER_LEARNED_DRAWER, mUserLearned + "");
+                }
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+        };
+        if (!mUserLearned && !mFromSavedInstanceState) {
+            drawerLayout.openDrawer(containerView);
+        }
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToggle.syncState();
+            }
+        });
+    }
+
+    public void hideNavigationDrawer()
+    {
+        mDrawerToggle.setDrawerIndicatorEnabled(false);
+        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+    }
+
+    public static void saveToPreference(Context context, String preferenceName, String preferenceValue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(preferenceName, preferenceValue);
+        editor.apply();
+    }
+
+    public static String readFromPreference(Context context, String preferenceName, String defaultValue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(preferenceName, defaultValue);
+    }
+
 
 }
