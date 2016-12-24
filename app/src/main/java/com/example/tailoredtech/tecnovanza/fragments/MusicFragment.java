56164436000fd29a828fd859aedb96d09ca2bed8
Najneen <najneen@tailoredtech.in>
2016-12-24 16:21:00 +0530

    dialog

--- app/src/main/java/com/example/tailoredtech/tecnovanza/fragments/MusicFragment.java
+++ app/src/main/java/com/example/tailoredtech/tecnovanza/fragments/MusicFragment.java
@@ -1,11 +1,16 @@
 package com.example.tailoredtech.tecnovanza.fragments;
 
+import android.content.DialogInterface;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 
 import com.example.tailoredtech.tecnovanza.R;
@@ -28,7 +33,16 @@ public class MusicFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_music, container, false);
+
         Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        spinner.getBackground().setColorFilter(ContextCompat.getColor(getActivity(),android.R.color.darker_gray), PorterDuff.Mode.SRC_ATOP);
+        ImageButton btnPlay = (ImageButton) view.findViewById(R.id.btn_play);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showWarningAlertWithPositiveBtn();
+            }
+        });
         // Spinner Drop down elements
         ArrayList<String> categories = new ArrayList<String>();
         categories.add(getString(R.string.txt_game));
@@ -47,6 +61,25 @@ public class MusicFragment extends Fragment {
 
 
         return view;
+    }
+
+    private void showWarningAlertWithPositiveBtn() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Google Music");
+        builder.setMessage("The Best Music Player since last 5 years");
+
+        String positiveText = "Dismiss";
+        builder.setPositiveButton(positiveText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        // display dialog
+        dialog.show();
     }
 
 }
