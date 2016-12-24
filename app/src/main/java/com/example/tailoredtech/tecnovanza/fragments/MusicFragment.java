dce4d0fc1d560ac7bc3cee3d755027cdbbb2cd18
Najneen <najneen@tailoredtech.in>
2016-12-24 14:21:32 +0530

    test

--- app/src/main/java/com/example/tailoredtech/tecnovanza/fragments/MusicFragment.java
+++ app/src/main/java/com/example/tailoredtech/tecnovanza/fragments/MusicFragment.java
@@ -5,9 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.example.tailoredtech.tecnovanza.R;
+
+import java.util.ArrayList;
 
 public class MusicFragment extends Fragment {
 
@@ -26,7 +29,20 @@ public class MusicFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_music, container, false);
         Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        // Spinner Drop down elements
+        ArrayList<String> categories = new ArrayList<String>();
+        categories.add("Automobile");
+        categories.add("Business Services");
+        categories.add("Computers");
+        categories.add("Education");
+        categories.add("Personal");
+        categories.add("Travel");
 
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         return view;
     }
